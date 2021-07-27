717ee57e727ebddfae3717a095e33e3bc625d64e
unknown <adrugarco18@protonmail.com>
2021-07-27 08:28:24 -0500

    SignUp

---
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
+  })
+  export class SignupComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+  }
