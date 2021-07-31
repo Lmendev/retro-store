bffc89dcb32399230a69bb115be63eaed37ae9af
vicrocsal <86812315+vicrocsal@users.noreply.github.com>
2021-07-31 09:07:09 -0500

    Agregar ruta estática a carpeta imágenes

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,7 +6,9 @@ app.set("port", process.env.PORT || 4000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 app.use("/api/v1/nfts", require("./src/routers/nftRoutes"));
 app.use("/api/v1/users", require("./src/routers/userRoutes"));
+app.use("/files", express.static(path.join("src/files")));
 
 module.exports = app;
