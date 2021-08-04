70fe0aa57873e7097d2034c7fea0176e527c15f7
vicrocsal <86812315+vicrocsal@users.noreply.github.com>
2021-08-04 11:47:48 -0500

    Agregar respuesta de servidor cuando la consulta no es satisfactoria

--- backend/src/controllers/nftController.js
+++ backend/src/controllers/nftController.js
@@ -2,59 +2,79 @@ const Nft = require("../models/nftModel");
 const nftCtrl = {};
 
 nftCtrl.listNft = async (req, res, next) => {
-  const nfts = await Nft.find();
-  res.json(nfts);
+  try {
+    const nfts = await Nft.find();
+    res.json(nfts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 nftCtrl.saveNft = async (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
-  const nft = new Nft({
-    title: req.body.title,
-    description: req.body.description,
-    image: url + "/files/" + req.file.filename,
-    token: req.body.token,
-    type: req.body.type,
-    price: req.body.price,
-    onSale: req.body.onSale,
-  });
+  try {
+    const url = req.protocol + "://" + req.get("host");
+    const nft = new Nft({
+      title: req.body.title,
+      description: req.body.description,
+      image: url + "/files/" + req.file.filename,
+      token: req.body.token,
+      type: req.body.type,
+      price: req.body.price,
+      onSale: req.body.onSale,
+    });
 
-  savedNft = await nft.save();
-  res.json(savedNft);
+    savedNft = await nft.save();
+    res.json(savedNft);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 nftCtrl.searchNft = async (req, res, next) => {
-  const { id } = req.params;
-  const nft = await Nft.findById(id);
-  res.json(nft);
+  try {
+    const { id } = req.params;
+    const nft = await Nft.findById(id);
+    res.json(nft);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 nftCtrl.updateNft = async (req, res, next) => {
-  let image = "";
-  if (req.file) {
-    const url = req.protocol + "://" + req.get("host");
-    image = url + "/files/" + req.file.filename;
-  } else {
-    image = req.body.image;
+  try {
+    let image = "";
+    if (req.file) {
+      const url = req.protocol + "://" + req.get("host");
+      image = url + "/files/" + req.file.filename;
+    } else {
+      image = req.body.image;
+    }
+
+    const nft = new Nft({
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      image: image,
+      token: req.body.token,
+      type: req.body.type,
+      price: req.body.price,
+      onSale: req.body.onSale,
+    });
+
+    updatedNft = await Nft.updateOne({ _id: req.params.id }, nft);
+    res.json(nft);
+  } catch (err) {
+    res.status(500).send(err);
   }
-
-  const nft = new Nft({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    image: image,
-    token: req.body.token,
-    type: req.body.type,
-    price: req.body.price,
-    onSale: req.body.onSale,
-  });
-
-  updatedNft = await Nft.updateOne({ _id: req.params.id }, nft);
-  res.json(nft);
 };
 
 nftCtrl.deleteNft = async (req, res, next) => {
-  await Nft.findByIdAndRemove(req.params.id);
-  res.json({ status: "NFT borrado." });
+  try {
+    await Nft.findByIdAndRemove(req.params.id);
+    res.json({ status: "NFT borrado." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 module.exports = nftCtrl;
