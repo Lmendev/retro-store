29b7261b4d2afc18095754ca42c97a235d03d519
vicrocsal <86812315+vicrocsal@users.noreply.github.com>
2021-08-04 13:13:50 -0500

    Backend development (#25)
    * Campos price y onSale en nftSchema
    
    * Creación de ruta y función de inicio de sesión
    
    * Creación de token de inicio de sesión y middleware NFTs
    
    * Almacenamiento de imágenes y guardado de rutas en base de datos
    
    * Cambios de esquema de usuario solicitado por vistas de Frontend
    
    * Respuesta de servidor con datos de NFT creado
    
    * Actualización de edición de NFTs con ruta de imágenes.
    
    * Agregar ruta estática a carpeta imágenes
    
    * Agregar respuesta de servidor cuando la consulta no es satisfactoria
    
    * Cambio de BD a región de AWS
    
    Co-authored-by: Luis Mendoza <lmendoza.mg@gmail.com>

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,46 +4,70 @@ const jwt = require("jsonwebtoken");
 const userCtrl = {};
 
 userCtrl.listUser = async (req, res, next) => {
-  const users = await User.find();
-  res.json(users);
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 userCtrl.saveUser = async (req, res, next) => {
-  req.body.password = Bcrypt.hashSync(req.body.password, 10);
-  const user = new User(req.body);
-  await user.save();
-  res.json({ status: "Usuario creado." });
+  try {
+    req.body.password = Bcrypt.hashSync(req.body.password, 10);
+    const user = new User(req.body);
+    await user.save();
+    res.json({ status: "Usuario creado." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 userCtrl.searchUser = async (req, res, next) => {
-  const { id } = req.params;
-  const user = await User.findById(id);
-  res.json(user);
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 userCtrl.updateUser = async (req, res, next) => {
-  const { id } = req.params;
-  await User.findByIdAndUpdate(id, { $set: req.body }, { new: true });
-  res.json({ status: "Usuario actualizado." });
+  try {
+    const { id } = req.params;
+    await User.findByIdAndUpdate(id, { $set: req.body }, { new: true });
+    res.json({ status: "Usuario actualizado." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 userCtrl.deleteUser = async (req, res, next) => {
-  await User.findByIdAndRemove(req.params.id);
-  res.json({ status: "Usuario borrado." });
+  try {
+    await User.findByIdAndRemove(req.params.id);
+    res.json({ status: "Usuario borrado." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 userCtrl.logIn = async (req, res, next) => {
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    return res.json({ status: "Usuario no existe" });
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({ status: "Usuario no existe" });
+    }
+    if (!Bcrypt.compareSync(req.body.password, user.password)) {
+      return res.json({ status: "La contraseña es incorrecta" });
+    }
+    const token = jwt.sign({ email: user.email, userID: user._id }, "secret", {
+      expiresIn: "1h",
+    });
+    res.json({ token: token, expiresIn: 3600, userId: user._id });
+  } catch (err) {
+    res.status(500).send(err);
   }
-  if (!Bcrypt.compareSync(req.body.password, user.password)) {
-    return res.json({ status: "La contraseña es incorrecta" });
-  }
-  const token = jwt.sign({ email: user.email, userID: user._id }, "secret", {
-    expiresIn: "1h",
-  });
-  res.json({ token: token, expiresIn: 3600, userId: user._id });
 };
 
 module.exports = userCtrl;
